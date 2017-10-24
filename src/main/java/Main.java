3e9e7f3ac5fb3ea0b5b18d5b6b2f877544cf91cc
Karl <37496339+aquarial@users.noreply.github.com>
2017-10-24 19:45:55 -0400

    Skeleton GUI complete

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,9 @@
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.Button;
+import javafx.scene.control.Tooltip;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -11,11 +13,37 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        StackPane root = new StackPane();
-        root.getChildren().add(new Label("Test"));
+        VBox button_box = new VBox();
+        button_box.setSpacing(40);
+        button_box.setPadding(new Insets(10));
 
+        Button load = new Button("Load");
+        load.setTooltip(new Tooltip("Load text file for participant data"));
+        load.setPrefSize(200, 50);
+        button_box.getChildren().add(load);
 
-        primaryStage.setScene(new Scene(root, 300, 250));
+        Button compute = new Button("Compute");
+        compute.setTooltip(new Tooltip("Compute a white elephant gift cycle"));
+        compute.setDisable(true);
+        compute.setPrefSize(200, 50);
+        button_box.getChildren().add(compute);
+
+        Button send = new Button("Send");
+        send.setTooltip(new Tooltip("Send emails!"));
+        send.setDisable(true);
+        send.setPrefSize(200, 50);
+        button_box.getChildren().add(send);
+
+        load.setOnAction(event -> {
+            compute.setDisable(false);
+            send.setDisable(true);
+        });
+        compute.setOnAction(event -> {
+            compute.setDisable(false);
+            send.setDisable(false);
+        });
+
+        primaryStage.setScene(new Scene(button_box));
         primaryStage.setTitle("White Elephant");
         primaryStage.show();
     }
