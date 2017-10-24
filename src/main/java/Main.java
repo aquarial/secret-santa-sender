0ddf4c7ff732d7f53e06153709be0e1386ab0a99
Karl <37496339+aquarial@users.noreply.github.com>
2017-10-24 19:28:30 -0400

    Use java8 and javafx template

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,22 @@
-public class Main {
-    public static void main(String[] args) {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
 
+public class Main extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        StackPane root = new StackPane();
+        root.getChildren().add(new Label("Test"));
+
+
+        primaryStage.setScene(new Scene(root, 300, 250));
+        primaryStage.setTitle("White Elephant");
+        primaryStage.show();
     }
 }
