e4dbefc33d172961928c06f66796e6efd627ca89
Karl <37496339+aquarial@users.noreply.github.com>
2017-10-24 20:58:11 -0400

    Parsing with error handling

--- src/main/java/Solver.java
+++ src/main/java/Solver.java
@@ -2,34 +2,57 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.NoSuchFileException;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Solver {
-    private static List<String> loadFileData() throws IOException {
-        return Files.readAllLines(Paths.get("data.txt"));
-    }
+    private static Maybe<List<Node>> loadFileData() {
 
-    @SuppressWarnings({"unused", "UnusedAssignment"})
-    public static void main(String[] args) {
+        List<String> data;
 
-        String errorMessage;
         try {
-
-            System.out.println(Solver.loadFileData());
+            data = Files.readAllLines(Paths.get("data.txt"));
 
         } catch (NoSuchFileException e) {
-
-            String path;
-            try {
-                path = Paths.get("data.txt").toAbsolutePath().toString();
-            } catch (Exception ignored) {
-                path = "data.txt";
-            }
-
-            errorMessage = "File: " + path + " does not exist!";
-
+            return new Maybe<>("error", "File: " + Paths.get("data.txt").toAbsolutePath().toString() + " does not exist!");
         } catch (IOException e) {
-            errorMessage = e.getLocalizedMessage();
+            return new Maybe<>("error", e.getLocalizedMessage());
         }
+
+        List<Node> people = new ArrayList<>();
+        boolean parseFailure = false;
+        List<String> failedParses = new ArrayList<>();
+
+        for (String d : data) {
+            if (d.length() == 0 || d.startsWith("#"))
+                continue;
+
+            try {
+                String[] info = d.split(", +");
+
+                Node parsed = new Node();
+                parsed.groupID = Integer.parseInt(info[0]);
+                parsed.name = info[1];
+                parsed.email = info[2];
+                people.add(parsed);
+            } catch (Exception ignored) {
+                parseFailure = true;
+                failedParses.add(d);
+            }
+        }
+
+        if (parseFailure) {
+            StringBuilder error = new StringBuilder();
+            for (String e : failedParses)
+                error.append("Failed to parse: \"").append(e).append("\"\n");
+
+            return new Maybe<>("error", error.toString());
+        }
+
+        return new Maybe<>(people);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(loadFileData());
     }
 }
