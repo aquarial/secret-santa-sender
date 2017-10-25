d5dcc048b1fc65e40d38211525f0f714ec4f3b51
Karl <37496339+aquarial@users.noreply.github.com>
2017-10-24 22:54:21 -0400

    Actually solve the problem with the buttons

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,20 +1,26 @@
 import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Tooltip;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import solver.Maybe;
+import solver.Solver;
+import solver.SolverFactory;
 
 public class Main extends Application {
     public static void main(String[] args) {
         launch(args);
     }
 
+    private Solver solver;
+
     @Override
     public void start(Stage primaryStage) {
         VBox button_box = new VBox();
-        button_box.setSpacing(40);
+        button_box.setSpacing(20);
         button_box.setPadding(new Insets(10));
 
         Button load = new Button("Load");
@@ -35,13 +41,35 @@ public class Main extends Application {
         button_box.getChildren().add(send);
 
         load.setOnAction(event -> {
-            compute.setDisable(false);
+            compute.setDisable(true);
             send.setDisable(true);
+
+            Maybe<Solver> ms = SolverFactory.parseSolver("data.txt");
+            if (ms.hadError) {
+                new Alert(Alert.AlertType.ERROR, ms.errorMessage).showAndWait();
+                solver = null;
+            } else {
+                solver = ms.result;
+                compute.setDisable(false);
+                compute.setText("Compute");
+            }
         });
         compute.setOnAction(event -> {
-            compute.setDisable(false);
             send.setDisable(false);
+            compute.setText("Recompute");
+            solver.generateSolution();
+            if (!solver.validSolution()) {
+                new Alert(Alert.AlertType.WARNING, "Someone in the gift exchange\n" +
+                        "will give to a person in their family!\n" +
+                        "\n" +
+                        "Recompute a solution or\n" +
+                        "change the groupings in data.txt").showAndWait();
+            }
         });
+
+
+
+
 
         primaryStage.setScene(new Scene(button_box));
         primaryStage.setTitle("White Elephant");
